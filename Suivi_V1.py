f594846d14593a635e01dd08c55768d18c47a60e
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-24 23:16:29 +0100

    Update Suivi_V1.py

--- Suivi_V1.py
+++ Suivi_V1.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 url = 'https://docs.google.com/spreadsheets/d/1qPhLKvm4BREErQrm0L38DcZFG4a-K0msSzARVIG_T_U/export?format=csv'
 df = pd.read_csv(url)
 
-# Trier le DataFrame par ordre croissant en fonction de la colonne "Poids (Kgs)"
-df = df.sort_values('Poids (Kgs)')
+# Transformer la colonne "Date" en objet datetime et trier le DataFrame par ordre croissant en fonction de la colonne "Poids (Kgs)"
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+df = df.sort_values(['Poids (Kgs)', 'Date'])
 
 # Afficher le graphique de l'évolution du poids par rapport à la date
 fig, ax = plt.subplots()
@@ -16,6 +17,7 @@ ax.set_xlabel('Date')
 ax.set_ylabel('Poids (Kgs)')
 ax.set_title('Evolution du poids')
 
-# Corriger l'axe des poids
+# Corriger l'axe des poids et des dates
 ax.set_ylim(bottom=0)  # La plage minimale est 0
+ax.invert_xaxis()  # Inverser l'axe des dates
 st.pyplot(fig)
