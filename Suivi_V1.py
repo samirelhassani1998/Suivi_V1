c9b90a13732c403491362bf7a4df95c7bdd37157
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-24 23:05:41 +0100

    Update Suivi_V1.py

--- Suivi_V1.py
+++ Suivi_V1.py
@@ -1,1 +1,16 @@
 
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# Charger le fichier CSV à partir du lien
+url = 'https://docs.google.com/spreadsheets/d/1qPhLKvm4BREErQrm0L38DcZFG4a-K0msSzARVIG_T_U/export?format=csv'
+df = pd.read_csv(url)
+
+# Afficher le graphique de l'évolution du poids par rapport à la date
+fig, ax = plt.subplots()
+ax.plot(df['Date'], df['Poids (Kgs)'])
+ax.set_xlabel('Date')
+ax.set_ylabel('Poids (Kgs)')
+ax.set_title('Evolution du poids')
+st.pyplot(fig)
