0323aefb1d20064c92ed186357ac7fcdd332abff
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-25 21:43:33 +0100

    Update Suivi_V1.py

--- Suivi_V1.py
+++ Suivi_V1.py
@@ -94,14 +94,9 @@ current_weight = df_filtered["Poids (Kgs)"].iloc[-1]
 weight_difference = target_weight - current_weight
 estimated_days_to_target = weight_difference / mean_change_rate
 
-# Calculer le déficit calorique ou l'excédent nécessaire pour atteindre l'objectif de poids
-caloric_difference = weight_difference * 7700 / estimated_days_to_target
-
-# Calculer les calories nécessaires pour maintenir le poids actuel
-calories_needed = bmr * activity_levels[activity_level]
-
 # Calculer les calories nécessaires pour atteindre l'objectif de poids
-calories_needed_to_reach_target = calories_needed + caloric_difference
+calories_needed_to_reach_target = calories_needed + (caloric_difference / estimated_days_to_target)
 
 st.write(f"Calories nécessaires pour atteindre l'objectif de poids : {calories_needed_to_reach_target:.0f} kcal par jour")
+
 
