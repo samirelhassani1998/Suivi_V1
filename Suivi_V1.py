6b027ed5cc1c3ca80b89bb30a900e1d3bc097c08
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-25 12:23:46 +0100

    Update Suivi_V1.py

--- Suivi_V1.py
+++ Suivi_V1.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import streamlit as st
+
+st.title("Evolution du poids")
 
 # Charger le fichier CSV à partir du lien
 url = 'https://docs.google.com/spreadsheets/d/1qPhLKvm4BREErQrm0L38DcZFG4a-K0msSzARVIG_T_U/export?format=csv'
@@ -20,31 +23,33 @@ df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y') # à adapter selon le
 df = df.sort_values('Date')
 
 poids_stats = df["Poids (Kgs)"].describe()
-print(poids_stats)
+st.write("Statistiques des poids :", poids_stats)
 
+fig1 = plt.figure()
 plt.plot(df["Date"], df["Poids (Kgs)"], marker="o")
 plt.xlabel("Date")
 plt.ylabel("Poids (Kgs)")
 plt.title("Evolution du poids")
-plt.show()
+st.pyplot(fig1)
 
 # Convertir les dates en nombres pour la régression linéaire
 df["Date_numeric"] = (df["Date"] - df["Date"].min()) / np.timedelta64(1, "D")
 
-# Entraîner le modèle de régression linéair
+# Entraîner le modèle de régression linéaire
 X = df[["Date_numeric"]]
 y = df["Poids (Kgs)"]
 reg = LinearRegression().fit(X, y)
 
 # Tracer la ligne de régression
+fig2 = plt.figure()
 plt.scatter(df["Date"], df["Poids (Kgs)"], label="Données")
 plt.plot(df["Date"], reg.predict(X), color="red", label="Régression linéaire")
 plt.xlabel("Date")
 plt.ylabel("Poids (Kgs)")
 plt.title("Evolution du poids avec régression linéaire")
 plt.legend()
-plt.show()
+st.pyplot(fig2)
 
 # Calculer le coefficient de détermination (R²)
 r_squared = reg.score(X, y)
-print(f"R²: {r_squared}")
+st.write(f"R²: {r_squared}")
