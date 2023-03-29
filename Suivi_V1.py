f315e9be722f250cd7dc6f727117db9b237f8fd9
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-29 22:55:00 +0200

    Update Suivi_V1.py

--- Suivi_V1.py
+++ Suivi_V1.py
@@ -22,7 +22,6 @@ st.title("Evolution du poids")
 url = 'https://docs.google.com/spreadsheets/d/1qPhLKvm4BREErQrm0L38DcZFG4a-K0msSzARVIG_T_U/export?format=csv'
 df = pd.read_csv(url, decimal=",")
 
-
 # Convertir la colonne "Poids (Kgs)" en nombres décimaux et remplacer les valeurs non numériques par NaN
 df['Poids (Kgs)'] = pd.to_numeric(df['Poids (Kgs)'], errors='coerce')
 
